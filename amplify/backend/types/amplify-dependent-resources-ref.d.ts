7a8d494c12e11834cf28f5cbcd6843b4aa6c8e81
begae <gdtmdf@gmail.com>
2023-03-04 02:33:21 +0900

    .

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quedabebe2bcf": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "quedastorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
