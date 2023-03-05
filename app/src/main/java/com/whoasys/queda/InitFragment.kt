6f45becb608df57315cb5ec45c1be22778746cb7
begae <gdtmdf@gmail.com>
2023-03-05 23:22:30 +0900

    init fragment 는 drop database queda 후에만 쓰세요..

--- app/src/main/java/com/whoasys/queda/InitFragment.kt
+++ app/src/main/java/com/whoasys/queda/InitFragment.kt
@@ -35,7 +35,10 @@ class InitFragment : Fragment() {
             35.85520714, 128.49362696
         )
 
-        val team = arrayOf(begae, kimsmj, hagaji, acctract)
+        val administrator = User("admin", "admin", "admin","admin@whoasys.com",
+            0.0, 0.0, false, null)
+
+        val team = arrayOf(begae, kimsmj, hagaji, acctract, administrator)
 
         lateinit var networkThread: Thread
         var name: String? = null
@@ -46,21 +49,20 @@ class InitFragment : Fragment() {
             for (member in team) {
                 networkThread = Thread {
                     name = NetworkService.call().join(member).execute().body()?.name
+                    print(name)
                     names = names.plus(name)
                 }
                 networkThread.start()
                 networkThread.join()
             }
 
-            println("$names 저장됨!")
+            print(" 저장됨!\n")
 
         }
 
-        val library = Store("계명대학교 성서캠퍼스 동산도서관",
-            "012-34-78901",
-            "대구 달서구 달구벌대로 1095",
-            35.856418, 128.487185, "053-580-5682",
-            "08:00 - 23:00")
+        val library = Store("계명대학교 성서캠퍼스 동산도서관", "012-34-78901",
+            "대구 달서구 달구벌대로 1095", 35.856418, 128.487185,
+            "053-580-5682", "08:00 - 23:00")
 
         val engineeringDept = Store("계명대학교 성서캠퍼스 공학관", "123-45-67890",
             "대구 달서구 달서대로 675", 35.85933090, 128.48701244,
@@ -74,7 +76,11 @@ class InitFragment : Fragment() {
             "대구 달서구 달구벌대로 1095", 35.85267052, 128.48889710,
             "053-580-6210", "09:00 - 18:00")
 
-        val school = arrayOf(library, engineeringDept, naturalScienceDept, mainStadium)
+        val adminStore = Store("오류가 발생했삼", "000-00-00000",
+            "대구 모처", 0.0, 0.0, "010-2789-3764",
+            "00:00 - 00:00", 0, null, null, false, 0)
+
+        val school = arrayOf(library, engineeringDept, naturalScienceDept, mainStadium, adminStore)
         var storeId: Int = 0
         var storeIds = emptyArray<Int>()
 
@@ -83,13 +89,14 @@ class InitFragment : Fragment() {
             for (facility in school) {
                 networkThread = Thread {
                     storeId = NetworkService.call().register(facility).execute().body()?: 0
-                    storeIds = storeIds.plus(storeId!!)
+                    print(storeId)
+                    storeIds = storeIds.plus(storeId)
                 }
                 networkThread.start()
                 networkThread.join()
             }
 
-            println("$storeIds 저장됨!")
+            print(" 저장됨!\n")
 
         }
 
@@ -101,13 +108,41 @@ class InitFragment : Fragment() {
             for (i: Int in team.indices) {
                 networkThread = Thread {
                     response = NetworkService.call().updateUserAsManager(team[i].id, storeIds[i]).execute().body()?: false
+                    print(response)
                     responses = responses.plus(response)
                 }
                 networkThread.start()
                 networkThread.join()
             }
 
-            println("업데이트 결과: $responses")
+            print("<- 업데이트 결과\n")
+
+        }
+
+        val postZero = Post("오류가 발생했삼", administrator, "관리자에게 문의하삼",
+            false, null, null, null, 0)
+
+        val postNegativeOne = Post("오류가 발생했삼", administrator, "관리자에게 문의하삼",
+            false, null, null, null, -1)
+
+        val postNegativeTwo = Post("오류가 발생했삼", administrator, "관리자에게 문의하삼",
+            false, null, null, null, -2)
+
+        val errorPosts = arrayOf(postZero, postNegativeOne, postNegativeTwo)
+        var postId: Int = -3
+
+        b.initPosts.setOnClickListener {
+
+            for (post in errorPosts) {
+                networkThread = Thread {
+                    postId = NetworkService.call().savePost(post).execute().body()?: -3
+                    print(postId)
+                }
+                networkThread.start()
+                networkThread.join()
+            }
+
+            print(" 게시물 생성함!\n")
 
         }
 
