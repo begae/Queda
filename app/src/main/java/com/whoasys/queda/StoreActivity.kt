c782cf3b82a68e4387155ebae683148850f434fd
begae <gdtmdf@gmail.com>
2023-03-08 17:21:29 +0900

    .

--- app/src/main/java/com/whoasys/queda/StoreActivity.kt
+++ app/src/main/java/com/whoasys/queda/StoreActivity.kt
@@ -1,28 +1,23 @@
 package com.whoasys.queda
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
-import com.whoasys.queda.databinding.StoreProfileBinding
+import com.whoasys.queda.databinding.ActivityStoreBinding
 import com.whoasys.queda.entities.NetworkService
 import com.whoasys.queda.entities.Store
 
-class StoreProfile : Fragment() {
+class StoreActivity : AppCompatActivity() {
 
     private var storeId: Int = 5
     private var store: Store? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            storeId = it.getInt("store_id")
-        }
 
         val networkThread = Thread {
             store = NetworkService.call().getOneStore(storeId.toString()).execute().body()
@@ -30,34 +25,21 @@ class StoreProfile : Fragment() {
 
         networkThread.start()
         networkThread.join()
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+        val b = ActivityStoreBinding.inflate(layoutInflater)
+        setContentView(b.root)
 
-        val b = StoreProfileBinding.inflate(layoutInflater)
-
+        val navHostFragment = b.storeNavContainer.getFragment<Fragment>()
+        val navController = navHostFragment.findNavController()
+        b.storeNavBar.setupWithNavController(navController)
 
         b.storeName.text = store!!.name
         b.storeAddress.text = store!!.address
         b.storePhone.text = store!!.phone
         b.storeOpenTime.text = store!!.openTime
 
-
-
-        return b.root
-    }
-
-    companion object {
-
-        @JvmStatic
-        fun newInstance(storeId: Int) =
-            StoreProfile().apply {
-                arguments = Bundle().apply {
-                    putInt("store_id", storeId)
-                }
-            }
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
     }
 }
