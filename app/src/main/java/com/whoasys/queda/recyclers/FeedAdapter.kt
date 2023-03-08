8f90405de18a307407ea10e58ecffcf0e2ec4e98
DESKTOP-6IKT6T7\KimSeoYeon <perte1101@gmail.com>
2023-03-08 23:00:53 +0900

    피드-피드리스트-피드어댑터

--- app/src/main/java/com/whoasys/queda/recyclers/FeedAdapter.kt
+++ app/src/main/java/com/whoasys/queda/recyclers/FeedAdapter.kt
@@ -4,18 +4,19 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import com.whoasys.queda.R
 
 import com.whoasys.queda.recyclers.PlaceholderContent.PlaceholderItem
 import com.whoasys.queda.databinding.FeedBinding
+import com.whoasys.queda.entities.Post
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
  * TODO: Replace the implementation with code for your data type.
  */
 class FeedAdapter(
-    private val values: List<PlaceholderItem>
+    private val itemList: ArrayList<Post>
 ) : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         return ViewHolder(
@@ -29,20 +30,42 @@ class FeedAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        holder.feed_name.text = itemList[position].author.store!!.name
+        holder.feed_location.text = itemList[position].author.store!!.address
+        //holder.feed_time.text = itemList[position].addedMillis.toString()
+        holder.feed_postTitle.text = itemList[position].title
+        //val imgUrl =
+        //holder.feed_postImg.setImageURI()
+        holder.feed_postId.text = itemList[position].id.toString()
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int{
+        return itemList.count()
+    }
 
     inner class ViewHolder(binding: FeedBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
+        val feed_name = itemView.findViewById<TextView>(R.id.feed_name)
+        val feed_location = itemView.findViewById<TextView>(R.id.feed_location)
+        val feed_time = itemView.findViewById<TextView>(R.id.feed_time)
+        val feed_postTitle = itemView.findViewById<TextView>(R.id.feed_postTitle)
+        //val tv_postImg = itemView.findViewById<ImageView>(R.id.feed_postImg)
+        val feed_postId = itemView.findViewById<TextView>(R.id.feed_postId)
 
         override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            return super.toString() //+ " '" + contentView.text + "'"
+        }
+        init {
+            itemView.setOnClickListener {
+                itemClickListener?.onItemClick(adapterPosition)
+            }
         }
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int) {}
+    }
+
+    var itemClickListener: OnItemClickListener? = null
+
 
 }
