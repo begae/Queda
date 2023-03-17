7d97ae7b4ce805410889336d9b5b53e9f29a8580
kimserion <gdtmdf@gmail.com>
2023-03-17 13:16:27 +0900

    ㅁㅣ완성. 절대로 구동 금지

---
+++ app/src/main/java/com/whoasys/queda/KeywordActivity.kt
@@ -0,0 +1,11 @@
+package com.whoasys.queda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KeywordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keyword)
+    }
+}
