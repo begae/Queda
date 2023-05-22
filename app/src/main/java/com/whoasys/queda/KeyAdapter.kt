6a53908132ebb985893d97fa1a2e981845d3e58e
HAENGCHO\acctr <acctract@naver.com>
2023-05-22 21:51:30 +0900

    매장등록 키워드에 아이템 뜨게하려고 시도중

--- app/src/main/java/com/whoasys/queda/KeyAdapter.kt
+++ app/src/main/java/com/whoasys/queda/KeyAdapter.kt
@@ -1,43 +1,43 @@
 package com.whoasys.queda
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.content.res.Resources
 import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat.getColor
+import coil.load
+import com.whoasys.queda.databinding.KeywordItemBinding
+import com.whoasys.queda.entities.BUCKET
+import com.whoasys.queda.entities.Keyword
 
-class KeyAdapter() : RecyclerView.Adapter<KeyAdapter.ItemViewHolder>() {
+class KeyAdapter(
+    private val itemList : List<Keyword>
+) : RecyclerView.Adapter<KeyAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-    private lateinit var itemList : ArrayList<KeywordModel>
-
-    constructor(itemList: ArrayList<KeywordModel>): this(){
-        this.itemList = itemList
+        return ViewHolder(
+            KeywordItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeyAdapter.ItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.keyword_items, parent, false)
-        return ItemViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: KeyAdapter.ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val item = itemList[position]
+        val selected = listOf<Keyword>()
 
-        holder.TextArea.text = item.name
-        holder.ImageArea.setImageDrawable(item.img)
-        holder.keyword_layout.setOnClickListener{
-            Toast.makeText(it.context, "${item.name}를/을 키워드로 선택했습니다.", Toast.LENGTH_SHORT).show()
-            setMultipleSelection(position)
-        }
+        holder.keywordName.text = item.value
+        holder.keywordImage.load(BUCKET + item.value + ".png")
 
-        if(item.selected){
-            holder.itemView.setBackgroundResource(R.drawable.radius6)
-        }
-        else{
-            holder.itemView.setBackgroundResource(R.drawable.radius5)
+        holder.item.setOnClickListener {
+
+            val green = getColor(Resources.getSystem(), R.color.teal_200, null)
+            holder.keywordImage.setBackgroundColor(green)
+
+            selected.plusElement(item)
         }
 
     }
@@ -46,16 +46,10 @@ class KeyAdapter() : RecyclerView.Adapter<KeyAdapter.ItemViewHolder>() {
         return itemList.size
     }
 
-    private fun setMultipleSelection(position: Int){
-        itemList[position].selected = !itemList[position].selected
+    inner class ViewHolder(binding: KeywordItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val keywordName = binding.keywordItemName
+        val keywordImage = binding.keywordItemThumb
+        val item = binding.keywordItem
 
-        notifyItemChanged(position)
     }
-
-    class ItemViewHolder(itemView : View): RecyclerView.ViewHolder(itemView){
-        val keyword_layout: LinearLayout = itemView.findViewById(R.id.keyword_layout)
-        val ImageArea :ImageView = itemView.findViewById(R.id.ImageArea)
-        val TextArea: TextView = itemView.findViewById(R.id.TextArea)
-    }
-
 }
--- app/src/main/java/com/whoasys/queda/KeywordChoice.kt
+++ app/src/main/java/com/whoasys/queda/KeywordChoice.kt
@@ -3,11 +3,12 @@ package com.whoasys.queda
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.provider.AlarmClock
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,13 +19,14 @@ class KeywordChoice : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyword_choice)
 
-        val userId = intent.getStringExtra(AlarmClock.EXTRA_MESSAGE)
+        val userId = intent.getStringExtra(EXTRA_MESSAGE)
 
-        val listkey = findViewById<RecyclerView>(R.id.listkey)
-        val adapter = KeyAdapter(getData())
 
-        listkey.layoutManager = GridLayoutManager(this, 3)
-        listkey.adapter = adapter
+       // val listkey = findViewById<RecyclerView>(R.id.listkey)
+       // val adapter = KeyAdapter(getData())
+
+        //listkey.layoutManager = GridLayoutManager(this, 3)
+        //listkey.adapter = adapter
 
         val saveBtn = findViewById<Button>(R.id.saveBtn)
         saveBtn.setOnClickListener {
@@ -33,46 +35,40 @@ class KeywordChoice : AppCompatActivity() {
                 .replace(R.id.keywordchoice, MyPage())
                 .addToBackStack(null)
                 .commit()
+//            val navController = Navigation.findNavController(this, R.id.keywordchoice)
+//            navController.navigate(R.id.Mypage)
         }
 
 
 
     }
 
-    private fun getData(): ArrayList<KeywordModel>{
-        val itemList: ArrayList<KeywordModel> = ArrayList()
-
-        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.coffee)!!, "차, 커피", false))
-        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.nail)!!, "네일아트", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.health)!!, "헬스장", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.store)!!, "편의점, 마트", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.hair)!!, "미용실", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.bathhouse)!!, "목욕탕", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.diningroom)!!, "음식점", false))
-//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.shirt)!!, "옷가게", false))
-
-        return itemList
-
-    }
+//    private fun getData(): ArrayList<KeywordModel>{
+//        val itemList: ArrayList<KeywordModel> = ArrayList()
+//
+//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.coffee)!!, "차, 커피", false))
+//        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.nail)!!, "네일아트", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.health)!!, "헬스장", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.store)!!, "편의점, 마트", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.hair)!!, "미용실", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.bathhouse)!!, "목욕탕", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.diningroom)!!, "음식점", false))
+////        itemList.add(KeywordModel(ContextCompat.getDrawable(this,R.drawable.shirt)!!, "옷가게", false))
+//
+//        return itemList
+//
+//    }
 
 
     override fun onBackPressed() {
         val builder= AlertDialog.Builder(this)
-        builder.setTitle("알림")
-        builder.setMessage("키워드 선택을 취소하시겠습니까?")
-        builder.setCancelable(false)
-        builder.setPositiveButton("확인", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {
-                exit()
-            }
-        })
-        builder.setNegativeButton("뒤로", object : DialogInterface.OnClickListener {
-            override fun onClick(dialog: DialogInterface?, which: Int) {}
-        })
-        builder.show()
+        .setTitle("알림")
+        .setMessage("키워드 선택을 취소하시겠습니까?")
+        .setCancelable(false)
+            .setPositiveButton("네") { dialog, id -> finish() }
+            .setNegativeButton("아니요", null)
+        val alert = builder.create()
+        alert.show()
     }
 
-    fun exit() {
-        super.onBackPressed()
-    }
 }
