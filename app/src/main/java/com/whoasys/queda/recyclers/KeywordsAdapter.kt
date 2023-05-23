9382dba1d32595d75e43841461f547dd7ffc84f1
HAENGCHO\acctr <acctract@naver.com>
2023-05-23 23:59:56 +0900

    내 매장 키워드 수정 -> 다중 선택 색상 변경 가능

--- app/src/main/java/com/whoasys/queda/recyclers/KeywordsAdapter.kt
+++ app/src/main/java/com/whoasys/queda/recyclers/KeywordsAdapter.kt
@@ -1,6 +1,5 @@
 package com.whoasys.queda.recyclers
 
-    import android.content.res.Resources
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -64,6 +63,10 @@ class KeywordsAdapter(
                 )
                 Toast.makeText(it.context, "${item.value}를/을 키워드로 선택했습니다.", Toast.LENGTH_SHORT).show()
             }
+
+            selected.plusElement(item)
+
+            uploadSelectedKeywords() // 선택된 키워드 업로드
         }
     }
 
@@ -77,5 +80,15 @@ class KeywordsAdapter(
         val keywordImage = binding.keywordItemThumb
         val item = binding.keywordItem
 
+    }
+
+    private fun uploadSelectedKeywords() {
+        // 선택된 키워드 업로드 처리
+        val selectedKeywordList = selectedItems.toList()
+
+        // TODO: 선택된 키워드를 업로드하는 로직을 구현하세요.
+        // 선택된 키워드 목록인 selectedKeywordList를 활용하여 업로드 작업을 수행하세요.
+        // 예를 들어, 선택된 키워드를 서버에 전송하거나 로컬 데이터베이스에 저장하는 등의 작업을 수행할 수 있습니다.
+        // 이 부분은 애플리케이션의 요구사항에 맞게 구현되어야 합니다.
     }
 }
